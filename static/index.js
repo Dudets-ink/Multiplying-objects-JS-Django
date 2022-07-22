de2112e30828e4a1544a768ad74bd0435c9f273a
Dima <pixel.art@bk.ru>
2022-07-22 10:29:13 +0300

    Добавил кнопку удаления

--- static/index.js
+++ static/index.js
@@ -1,3 +1,6 @@
+/*
+
+*/
 function addForm(){
     const button = document.getElementById('buttonAddForm');
 
@@ -5,13 +8,24 @@ function addForm(){
         const inputClass = document.getElementById('input-form');
         const inputForm = document.getElementById('empty-input').cloneNode(true);
         const currentFormCount = document.getElementsByClassName('input-class').length;
-        const totalFormsets = document.getElementById('total-formsets');
         const regex = new RegExp('__prefix__', 'g')
         inputForm.innerHTML = inputForm.innerHTML.replace(regex, currentFormCount-1)
 
         inputForm.setAttribute('class', 'input-class');
         inputForm.setAttribute('id', `form-${currentFormCount}`);
         inputClass.append(inputForm);
-        totalFormsets.setAttribute('value', currentFormCount)
     });
 };
+
+/*
+
+*/
+function deleteForm(){
+    const button = document.getElementById('buttonDelForm');
+
+    button.addEventListener("click", function(){
+        const inputClass = document.getElementById('input-form');
+
+        inputClass.removeChild(inputClass.lastChild);
+    });
+}
